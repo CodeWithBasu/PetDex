833a8b3d580fb9e497ec32d534b3ab42f58e6409
Basudev Moharana <basudevmuna111@gmail.com>
2026-07-30 15:53:14 +0530

    fix: add public/sw.js service worker file to prevent 404 logs

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+// Service Worker dummy fallback to handle browser / extension PWA sw.js requests.
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
